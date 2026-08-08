b5cf9a7ab336a95f94f8c11c0b6748ae8c72fd54
ASenoJati <senojati16@gmail.com>
2026-08-08 15:22:40 +0700

    Feat [SEN] footer edit

--- resources/views/layouts/admin/footer.blade.php
+++ resources/views/layouts/admin/footer.blade.php
@@ -1,12 +1,12 @@
  <footer class="app-footer">
      <!--begin::To the end-->
-     <div class="float-end d-none d-sm-inline">TA RPL RUS</div>
+     {{-- <div class="float-end d-none d-sm-inline">TA RPL RUS</div> --}}
      <!--end::To the end-->
      <!--begin::Copyright-->
      <strong>
          Copyright &copy; 2026&nbsp;
-         <a href="https://adminlte.io" class="text-decoration-none">KELOMPOK 14</a>
+         {{-- <a href="https://adminlte.io" class="text-decoration-none">KELOMPOK 14</a> --}}
      </strong>
-     Tugas Akhir PPLG RUS.
+     SD Cahaya Nur
      <!--end::Copyright-->
  </footer>
